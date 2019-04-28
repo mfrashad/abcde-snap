40d440a43c1ed1cb62659f0492ec7906788d93e5
mfrashad <m.fathyrashad@gmail.com>
2019-04-28 10:09:52 +0800

    Create json data api

--- app/controllers/datum_controller.rb
+++ app/controllers/datum_controller.rb
@@ -16,5 +16,13 @@ class DatumController < ApplicationController
     @headers = csv.headers
     @rows = csv.take(10)
   end
+
+  def data
+    csv_text = File.read('public/uploads/datum/cleaner.csv')
+    csv = CSV.parse(csv_text, :headers => true)
+    @headers = csv.headers
+    @rows = csv.take(10)
+    render :json => @rows
+  end
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  resources :datum, only: [:create,:index]
+  resources :datum do
+    collection do 
+      get :data
+    end
+  end
   #get '/:static_page', to: 'pages#show'
 end
